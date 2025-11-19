2a84a8ee61474cc09394d3048e478e01b740c09b
SpragueWandsworth <thetelescope46@gmail.com>
2025-11-19 15:55:27 +0000

    Added getComputerChoice logic

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-console.log("mesa")
+
+//get random number between 0 and 1
+//choose apply either rock, paper or scissors based on that number to result
+
+function getComputerChoice() {
+    let randomNumber = Math.random()
+    let result = undefined
+
+    if (randomNumber >= 0 && randomNumber < 0.34) {
+        result = "Rock"
+    }
+    else if (randomNumber >= 0.34 && randomNumber < 0.67) {
+        result = "Paper"
+    }
+    else if (randomNumber >= 0.67 && randomNumber <=1) {
+        result = "Scissors"
+    }
+    console.log(result)
+}
+
+getComputerChoice()
