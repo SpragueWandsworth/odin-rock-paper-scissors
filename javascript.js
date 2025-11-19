17523f23114b9d6c028db8c70e32ac8c09f55ccc
SpragueWandsworth <thetelescope46@gmail.com>
2025-11-19 20:46:51 +0000

    Added playRound logic and scoring

--- javascript.js
+++ javascript.js
@@ -19,10 +19,9 @@ function getComputerChoice() {
     else if (randomNumber >= 0.67 && randomNumber <=1) {
         result = "scissors";
     }
-    console.log(result);
+    console.log("computer picked " + result);
     return result;
 }
-getComputerChoice();
 
 //prompt user
 //get users input
@@ -31,21 +30,49 @@ getComputerChoice();
 
 function getPlayerChoice() {
     let playerInput = prompt("Choose either rock, paper or scissors!").toLowerCase();
-    console.log(playerInput);
-    return playerInput
+    console.log("player picked " + playerInput);
+    return playerInput;
 }
-
-getPlayerChoice();
 
 //get computer choice
 //get human choice
 //compare choices
+//assign winner
+//add one point to the winner
 
-function playRound(playerChoice, computerChoice) {
-    console.log(playerChoice + computerChoice)
+function playRound(userChoice, computerChoice) {
+    let winner = undefined;
+        if (computerChoice === "rock" ) {
+            if (userChoice === "rock"){winner = "draw"}
+            else if (userChoice === "scissors"){winner = "computer"}
+            else if (userChoice === "paper"){winner = "player"};
+        }
+        else if (computerChoice === "paper" ) {
+            if (userChoice === "paper"){winner = "draw"}
+            else if (userChoice === "rock"){winner = "computer"}
+            else if (userChoice === "scissors"){winner = "player"};
+        }
+        else if (computerChoice === "scissors" ) {
+            if (userChoice === "scissors"){winner = "draw"}
+            else if (userChoice === "paper"){winner = "computer"}
+            else if (userChoice === "rock"){winner = "player"};
+        }
+
+        if (winner === "player") {
+            playerScore += 1;
+        }
+        else if (winner === "computer") {
+            computerScore += 1;
+        }
+
+        console.log(winner + " won the game.");
+        console.log("the score is now " + playerScore + " to player, and " + computerScore + " to the computer.")
 }
 
 const computerChoice = getComputerChoice();
 const playerChoice = getPlayerChoice();
 
 playRound(playerChoice, computerChoice);
+
+getComputerChoice();
+getPlayerChoice();
