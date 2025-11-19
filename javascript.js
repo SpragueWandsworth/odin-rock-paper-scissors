0249a945f41763bea3d9a73c4e4e27a3a318f37a
SpragueWandsworth <thetelescope46@gmail.com>
2025-11-19 16:05:37 +0000

    Started getPlayerChoice

--- javascript.js
+++ javascript.js
@@ -3,19 +3,29 @@
 //choose apply either rock, paper or scissors based on that number to result
 
 function getComputerChoice() {
-    let randomNumber = Math.random()
-    let result = undefined
+    let randomNumber = Math.random();
+    let result = undefined;
 
     if (randomNumber >= 0 && randomNumber < 0.34) {
-        result = "Rock"
+        result = "Rock";
     }
     else if (randomNumber >= 0.34 && randomNumber < 0.67) {
-        result = "Paper"
+        result = "Paper";
     }
     else if (randomNumber >= 0.67 && randomNumber <=1) {
-        result = "Scissors"
+        result = "Scissors";
     }
-    console.log(result)
+    console.log(result);
+    return result;
+}
+getComputerChoice()
+
+//prompt user
+//get users input
+
+function getPlayerChoice() {
+    let playerInput = prompt("Choose either rock, paper or scissors!");
+    console.log(playerInput)
 }
 
-getComputerChoice()
+getPlayerChoice();
